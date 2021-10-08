ceacd8e1ccdeee76f03eeff55b18175766c8299a
alexa <alex@gmail.com>
2021-10-08 11:04:44 +0300

    deploy6

--- config/storybook/main.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  stories: ['../../src/components/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-links', '@storybook/addon-knobs']
-}
--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
-  transform: {
-    '^.+\\.vue$': 'vue-jest'
-  },
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/(*.)+(spec|test).[jt]s?(x)']
-}
